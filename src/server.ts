7755cbf1a66cdc243556ea77f1b3a53d2d12d187
Abenu10 <abmlengineer@gmail.com>
2024-01-27 06:17:08 -0800

    Middlwares

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,26 @@
 import express from 'express';
 import router from './router';
+import morgan from 'morgan';
+import cors from 'cors';
 
 const app = express();
 
+const customLogger = (message) => (req, res, next) => {
+  console.log(`hello from ${message}`);
+  next();
+};
+
+// Middleware
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(customLogger('morgan'));
+
+app.use((req, res, next) => {
+  req.shhh_secret = 'doggy';
+  next();
+});
 app.get('/', (req, res) => {
   console.log('hello from express');
   res.status(200);
