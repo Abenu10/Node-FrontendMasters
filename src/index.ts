7e068605edbee60c604fe2b7a9534a49436d02a7
Abenu10 <abmlengineer@gmail.com>
2024-02-01 08:54:08 -0800

    environment configuration

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import * as dotenv from 
 'dotenv'
 dotenv.config();
+import config from './config';
 
 import app from './server';
 
-const PORT = process.env.PORT || 3001;
+// const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => {
-  console.log('hello on port 3001');
+// app.listen(PORT, () => {
+//   console.log('hello on port 3001');
+// });
+app.listen(config.port, () => {
+  console.log(`hello on http://localhost:${config.port}`);
 });
